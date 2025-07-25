ad1039a35bf33f194a2654d22ace8c00f55290f5
Litopolis <59971991+AngelRiosLito@users.noreply.github.com>
2025-07-25 15:10:43 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Fergeda_2023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bunifuImageButton3 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt_Contraseña = new System.Windows.Forms.TextBox();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bunifuGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Inmobiliaria Fergeda";
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.bunifuImageButton1);
+            this.bunifuGradientPanel1.Controls.Add(this.label4);
+            this.bunifuGradientPanel1.Controls.Add(this.bunifuImageButton3);
+            this.bunifuGradientPanel1.Controls.Add(this.pictureBox1);
+            this.bunifuGradientPanel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(148)))), ((int)(((byte)(178)))));
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.MidnightBlue;
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(169)))), ((int)(((byte)(199)))));
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(80)))), ((int)(((byte)(166)))));
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(209, 0);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(179, 183);
+            this.bunifuGradientPanel1.TabIndex = 22;
+            this.bunifuGradientPanel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.bunifuGradientPanel1_MouseDown);
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(50, 156);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(23, 23);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 35;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkGray;
+            this.label4.Location = new System.Drawing.Point(121, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "V. 25.04.16";
+            // 
+            // bunifuImageButton3
+            // 
+            this.bunifuImageButton3.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton3.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton3.Image")));
+            this.bunifuImageButton3.ImageActive = null;
+            this.bunifuImageButton3.Location = new System.Drawing.Point(153, 3);
+            this.bunifuImageButton3.Name = "bunifuImageButton3";
+            this.bunifuImageButton3.Size = new System.Drawing.Size(23, 23);
+            this.bunifuImageButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton3.TabIndex = 32;
+            this.bunifuImageButton3.TabStop = false;
+            this.bunifuImageButton3.Zoom = 10;
+            this.bunifuImageButton3.Click += new System.EventHandler(this.bunifuImageButton3_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(179, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 33;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_Contraseña
+            // 
+            this.txt_Contraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Contraseña.Font = new System.Drawing.Font("Arial Narrow", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.txt_Contraseña.ForeColor = System.Drawing.Color.DarkGray;
+            this.txt_Contraseña.Location = new System.Drawing.Point(31, 110);
+            this.txt_Contraseña.MaxLength = 10;
+            this.txt_Contraseña.Name = "txt_Contraseña";
+            this.txt_Contraseña.Size = new System.Drawing.Size(134, 22);
+            this.txt_Contraseña.TabIndex = 29;
+            this.txt_Contraseña.Text = "Contraseña";
+            this.txt_Contraseña.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_Contraseña.Enter += new System.EventHandler(this.txt_Contraseña_Enter);
+            this.txt_Contraseña.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Contraseña_KeyPress);
+            this.txt_Contraseña.Leave += new System.EventHandler(this.txt_Contraseña_Leave);
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_usuario.Font = new System.Drawing.Font("Arial Narrow", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.txt_usuario.ForeColor = System.Drawing.Color.DarkGray;
+            this.txt_usuario.Location = new System.Drawing.Point(31, 64);
+            this.txt_usuario.MaxLength = 15;
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(134, 22);
+            this.txt_usuario.TabIndex = 28;
+            this.txt_usuario.Text = "Usuario";
+            this.txt_usuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_usuario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_usuario_KeyPress);
+            this.txt_usuario.Leave += new System.EventHandler(this.txt_usuario_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label2.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label2.Location = new System.Drawing.Point(21, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 20);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "_____________________";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label3.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label3.Location = new System.Drawing.Point(19, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 20);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "_____________________";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(388, 183);
+            this.Controls.Add(this.bunifuGradientPanel1);
+            this.Controls.Add(this.txt_Contraseña);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.bunifuGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.TextBox txt_Contraseña;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using Fergeda_2023.Clases;
+using Fergeda_2023.Compras;
+using Fergeda_2023.Compras.PDF;
+using Fergeda_2023.Finanzas;
+using Fergeda_2023.General;
+using Fergeda_2023.Mantenimiento_Edificio;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Fergeda_2023
+{
+    public partial class Form1 : Form
+    {
+        private estiloVentana est;
+        private Consultas X;
+        private Info info;
+        private MensageC msg;
+        public Form1()
+        {
+            est = new estiloVentana();
+            X = new Consultas();
+            info = new Info();
+            msg = new MensageC();
+            InitializeComponent();
+            est.estilo(this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new CuentasPagar(2647,2).ShowDialog();
+            //new CuentasPagar(749, 4).Show();
+            //new SolicitudChequePDF().generarCheque("30");
+        }
+
+        private void bunifuImageButton3_Click(object sender, EventArgs e)
+        {
+            //new GenerarOCF().generarOC(3);
+        }
+
+        private void bunifuImageButton3_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+        private void bunifuGradientPanel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        bool accesos = false;
+        private void txt_usuario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = char.IsWhiteSpace(e.KeyChar);
+            if (txt_usuario.Text == "Usuario")
+            {
+                txt_usuario.Text = "";
+                txt_usuario.ForeColor = Color.Black;
+                accesos = true;
+            }
+        }
+
+        private void txt_usuario_Leave(object sender, EventArgs e)
+        {
+            if (txt_usuario.Text == "")
+            {
+                txt_usuario.Text = "Usuario";
+                txt_usuario.ForeColor = Color.DarkGray;
+            }
+        }
+
+        private void txt_Contraseña_Leave(object sender, EventArgs e)
+        {
+            if (txt_Contraseña.Text == "")
+            {
+                txt_Contraseña.Text = "Contraseña";
+                txt_Contraseña.ForeColor = Color.DarkGray;
+                txt_Contraseña.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void txt_Contraseña_Enter(object sender, EventArgs e)
+        {
+            if (txt_Contraseña.Text == "Contraseña")
+            {
+                accesos = true;
+                txt_Contraseña.Text = "";
+                txt_Contraseña.ForeColor = Color.Black;
+                txt_Contraseña.UseSystemPasswordChar = true;
+            }
+        }
+        #region PERMISOS
+        public string permisos()
+        {
+            string tip;
+            tip = X.elem3("SELECT if(count(Permiso1)=0,'XuCbeXiOsCk=',Permiso1) FROM fergeda.s_pass where empleado='" + Convert.ToInt32(txt_usuario.Text) + "';");
+            tip = new Transformacion().Mostrar(tip, new Transformacion().Formato);
+            if (!tip.Equals("No"))            
+                if (X.elem3("SELECT Estatus FROM fergeda.s_pass where empleado='" + Convert.ToInt32(txt_usuario.Text) + "'").Equals("Bloqueado"))                
+                   tip = "No";
+            return tip;
+        }
+
+        public bool contraseña()
+        {
+            bool ac = false;         
+            if (new Transformacion().Mostrar(X.elem3("SELECT pass FROM fergeda.s_pass where empleado='" + Convert.ToInt32(txt_usuario.Text) + "';"), new Transformacion().Formato).Equals(txt_Contraseña.Text))            
+                ac = true;            
+            else  ac = false; 
+
+            return ac;
+        }
+
+        #endregion
+        int contador;
+        private void txt_Contraseña_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int dat = 0;
+            e.Handled = char.IsWhiteSpace(e.KeyChar);
+            if ((int)e.KeyChar == (int)Keys.Enter)
+            {
+                int dep = info.PermisoApp(Convert.ToInt32(txt_usuario.Text));
+              
+                if (permisos() != "No" && contraseña() == true)
+                {
+                    info.accesos(txt_usuario.Text + "/Activo", dep);
+                    dat++;
+                    switch (dep)
+                    {
+                        case 4:
+                            new MenuFinanzas(Convert.ToInt32(txt_usuario.Text)).Show();
+                            Hide();
+                            break;
+                        case 6:
+                            MenuCompras n = new MenuCompras(Convert.ToInt32(txt_usuario.Text));
+                            n.Show();
+                            Hide();
+                            break;
+                        case 7:
+                            MenuMantoEdificio n1 = new MenuMantoEdificio(Convert.ToInt32(txt_usuario.Text));
+                            n1.Show();
+                            Hide();
+                            break;
+                    }                   
+                }
+                else
+                {
+                    contador++;
+                    if (contador < 4)
+                    {
+                        msg.cargarEl("information", "Incorrecto", "Usuario o contraseña Incorrecta"); msg.Visible = true;
+                        info.accesos(txt_usuario.Text + "/Denegado", dep);
+                        txt_Contraseña.Text = "";
+                    }
+                    else
+                    {
+                        msg.cargarEl("lock(1)", "Acceso Denegado", "La cuenta se a Bloqueado Comuniquese\ncon el Administrador"); msg.ShowDialog();                       
+                            X.insertar("UPDATE fergeda.s_pass SET Estatus='Bloqueado' WHERE empleado='" + Convert.ToInt32(txt_Contraseña.Text) + "';");/*Cambiar a bloqueado*/
+                        info.accesos(txt_usuario.Text + "/Bloqueada", dep);
+                    }
+                }
+            }
+            
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            new RequisicionCompra(3453).Show();
+            //new requisicioncompra().LlenadoPdf(11);
+            //new MenuCompras(2940).ShowDialog();
+            //new chequeAsignado().Show();
+            //new CuentasCobradas(2647,4).Show();
+        }
+    }
+}
